7c0b86776bd3bc0e690e181d62332d5cbcebbd1e
nachiket012 <89441920+nachiket012@users.noreply.github.com>
2022-11-30 22:06:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ def main():
   st.title("Subtraction")
   html_temp = """
   <div style="background-color:tomato;padding:10px">
-  <h2 style="color:white;text-align:center;">Subtraction of 2 numbers using Streamlit</h2>
+  <h2 style="color:white;text-align:center;">Multiplication of 2 numbers using Streamlit</h2>
   </div>
   """
   st.markdown(html_temp,unsafe_allow_html=True)
   num1 = st.number_input("Number 1")
   num2 = st.number_input("Number 2")
-  result = num1 - num2
+  result = num1 * num2
   st.success('The output is {}'.format(result))
   if st.button("Made By"):
-      st.text("Parth Bansal")
-      st.text("21f3000805")
+      st.text("Nachiket Dandegaonkar")
+      st.text("21f1006205")
 
 if __name__=='__main__':
   main()
